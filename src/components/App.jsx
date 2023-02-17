ba3bc5299ee2cf3b03d04ec79af0e03f272b7b8b
Vladyslav Marushchak <marushchakvl@icloud.com>
2023-02-17 21:22:31 +0200

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+export class App extends Component {
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+  onLeaveFeedback = e => {
+    // console.log(e.target)
+    // console.log(e.currentTarget)
+    const name = e.target.name;
+    this.setState(prevState => {
+      return {
+        [name]: prevState[name] + 1
+      }
+    })
+  }
+  countTotalFeedback = () => {
+    const {good, neutral, bad} = this.state
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const {good, neutral, bad} = this.state
+    if (good > 0 || neutral > 0 || bad > 0) {
+      return ((good / (good + neutral + bad)) * 100).toFixed(0);
+    } else {
+      return 0;
+    }
+  }
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback
+    return <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions 
+          options={Object.keys(this.state)} 
+          onLeaveFeedback={this.onLeaveFeedback}/>
+      </Section>
+       <Section title="Statistics">
+        
+       {total() ? (<Statistics 
+          good={good} 
+          neutral={neutral} 
+          bad={bad} 
+          total={total} 
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />):
+          (<Notification message="There is no feedback"/>)
+       }
+      </Section>
+
+
+    </>
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  margin-right:12px;
+  background-color: #85e6ed;
+  border: 1px solid black;
+  border-radius: 3px;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 14px;
+  height: 29px;
+  padding: 0 10px;
+`
